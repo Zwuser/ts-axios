4780b70b76050a4744be76ae24f4aaa4d158a4b6
Zw <1983403952@qq.com>
2020-01-26 21:16:00 +0800

    feat: 初始化

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["airbnd-base","plugin:prettier/recommended","plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {}
+};
+
